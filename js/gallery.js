0fe630f8853e5b4933a3463c82b87c6d77028623
FernandoFeN <fernandofenosv@gmail.com>
2022-01-19 15:48:56 -0600

    desktop version finished

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,7 @@
 const previewBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
 const gallery = document.querySelector(".gallery__img");
+const paragraph = document.querySelector(".info-content");
 let counter = 0;
 
 const prev = () => {
@@ -8,8 +9,10 @@ const prev = () => {
         return;
     };
     counter--;
-    gallery.children[0].classList.remove("appear");
+    clearItems();
     gallery.children[0].classList.add("disappear");
+    paragraph.children[0].classList.add("appear");
+    paragraph.children[1].classList.add("disappear");
     gallery.children[1].classList = "active-prev";
 }
 
@@ -18,10 +21,20 @@ const next = () => {
         return;
     };
     counter++;
-    gallery.children[0].classList.remove("disappear");
+    clearItems();
     gallery.children[0].classList.add("appear");
+    paragraph.children[1].classList.add("appear");
+    paragraph.children[0].classList.add("disappear");
     gallery.children[1].classList = "active-next";
+}
+
+const clearItems = () => {
+    for(let i = 0; i < 2; i++) {
+        gallery.children[i].classList = "";
+        paragraph.children[i].classList = "testimonial";
+    }
 }
 
 previewBtn.addEventListener("click", prev);
 nextBtn.addEventListener("click", next);
+
